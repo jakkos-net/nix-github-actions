88699aaea094c66b497225e716540b02cb137758
Jak <45759112+jakkos-net@users.noreply.github.com>
2023-08-15 18:43:10 +0100

    auto

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-fn main(){
+fn main() {
     println!("1 + 1 = {}", calc());
 }
 
-fn calc() -> u32{
+fn calc() -> u32 {
     1 + 1
 }
 
 #[cfg(test)]
 mod tests {
     use crate::calc;
-   #[test]
-   fn test_calc(){
+    #[test]
+    fn test_calc() {
         assert_eq!(calc(), 2);
-   }
+    }
 }
